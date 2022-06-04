927dc572ad2dcb2758484897e1fc09f4de3506ed
Baboo7 <dbsharp404@gmail.com>
2022-06-05 01:21:29 +0200

    fix: Log error stack trace

--- server/bootstrap/wrap-logger.js
+++ server/bootstrap/wrap-logger.js
@@ -17,11 +17,21 @@ const wrapLogger = (strapi) => {
   for (const method of methods) {
     const original = strapi.log[method];
 
+    if (!original) {
+      continue;
+    }
+
+    const msgIdx = method === "log" ? 1 : 0;
+
     strapi.log[method] = function (...args) {
       const session = getNamespace("logger");
 
       const correlationId = session?.get("correlationId") || "-";
       const requestId = session?.get("requestId") || "-";
+
+      if (args[msgIdx] instanceof Error) {
+        args[msgIdx] = args[msgIdx].stack;
+      }
 
       return original.apply(strapi.log, [
         ...args,
